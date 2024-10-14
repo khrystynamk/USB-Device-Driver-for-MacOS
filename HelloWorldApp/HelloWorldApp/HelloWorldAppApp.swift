a934f9f01482588b566fd6d217621df8db3285f2
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2024-10-14 14:49:48 +0300

    Uploaded 'HelloWorld' XCode project

---
+++ HelloWorldApp/HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by Mariia on 11.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloWorldApp/HelloWorldAppTests/HelloWorldAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppTests.swift
+//  HelloWorldAppTests
+//
+//  Created by Mariia on 11.10.2024.
+//
+
+import Testing
+@testable import HelloWorldApp
+
+struct HelloWorldAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
