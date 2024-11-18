a67a4622140f67e97dae2e6e2ead3ced20c2a3ab
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2024-11-18 17:21:30 +0200

    Add files via upload

---
+++ USBHIDDriverApp/USBHIDDriverApp/USBHIDDriverAppApp.swift
@@ -0,0 +1,17 @@
+////
+////  USBHIDDriverAppApp.swift
+////  USBHIDDriverApp
+////
+////  Created by Mariia on 16.11.2024.
+////
+//
+//import SwiftUI
+//
+//@main
+//struct USBHIDDriverAppApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
---
+++ USBHIDDriverApp/USBHIDDriverAppTests/USBHIDDriverAppTests.swift
@@ -0,0 +1,17 @@
+////
+////  USBHIDDriverAppTests.swift
+////  USBHIDDriverAppTests
+////
+////  Created by Mariia on 16.11.2024.
+////
+//
+//import Testing
+//@testable import USBHIDDriverApp
+//
+//struct USBHIDDriverAppTests {
+//
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
+//
+//}
